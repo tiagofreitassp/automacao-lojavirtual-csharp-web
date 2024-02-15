4ce3faab68f31822276d6c200c60b2fc2fa84efa
Tiago Freitas <tiago.freitas.sp@gmail.com>
2024-02-15 10:19:43 -0300

    Criado projeto automacao-lojavirtual-csharp-web

---
+++ automacao-lojavirtual-csharp-web/automacao-lojavirtual-csharp-web/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace automacao_lojavirtual_csharp_web;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
