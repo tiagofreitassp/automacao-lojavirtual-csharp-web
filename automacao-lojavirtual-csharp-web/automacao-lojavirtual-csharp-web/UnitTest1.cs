83617917b426967d230177a387c1cd7411c9adaf
Tiago Freitas <tiago.freitas.sp@gmail.com>
2024-02-15 11:29:36 -0300

    Criado Teste na classe UnitTest1

--- automacao-lojavirtual-csharp-web/automacao-lojavirtual-csharp-web/UnitTest1.cs
+++ automacao-lojavirtual-csharp-web/automacao-lojavirtual-csharp-web/UnitTest1.cs
@@ -1,18 +1,50 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace automacao_lojavirtual_csharp_web;
 
 public class Tests
 {
     public IWebDriver driver;
+    private WebDriverWait wait;
 
     [SetUp]
     public void Setup()
-    {}
+    {
+        new DriverManager().SetUpDriver(new EdgeConfig());
+        driver = new EdgeDriver();
+
+        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+        driver.Manage().Window.Maximize();
+        driver.Navigate().GoToUrl("http://www.automationpractice.pl/index.php");
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+        Console.WriteLine("Driver iniciado com sucesso!");
+    }
 
     [Test]
-    public void Test1()
+    public void Teste()
     {
-        Assert.Pass();
+        IWebElement el1 = wait.Until(ExpectedConditions.ElementToBeClickable(By.ClassName("login")));
+        el1.Click();
+
+        IWebElement el2 = wait.Until(ExpectedConditions.ElementToBeClickable(By.Name("email")));
+        el2.SendKeys("teste@mail.com");
+
+        IWebElement el3 = wait.Until(ExpectedConditions.ElementToBeClickable(By.Name("passwd")));
+        el3.SendKeys("Senh@CSharp");
+
+        Thread.Sleep(3000);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        driver.Close();
+        Console.WriteLine("Driver finalizado com sucesso!");
     }
 }
